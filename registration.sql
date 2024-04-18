f9d3160353f044f1fcd36d461b40ec54be5badb7
BHAVADHARANH <117972525+BHAVADHARANH@users.noreply.github.com>
2024-04-18 21:51:41 +0530

    Add files via upload

---
+++ registration.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 18, 2024 at 02:16 PM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `registration`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE IF NOT EXISTS `data` (
+  `pid` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `age` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  PRIMARY KEY (`pid`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `data`
+--
+
+INSERT INTO `data` (`pid`, `name`, `age`, `address`, `contact`, `mail`) VALUES
+(1, 'dharani', '24\r\n', 'salem', '9080013465', 'dharani@gmail.com'),
+(2, 'sathya', '24', 'salem\r\n', '8765432109', 'sathya@gmail.com'),
+(3, 'rahul', '23', 'trichy', '8976542341', 'rahul@gmail.com');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
